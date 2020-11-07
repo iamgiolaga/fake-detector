5a4e7919cceba6a6875d05561e9fc9447e28a5b5
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2020-11-07 19:41:41 +0100

    improved plots

--- main_learning.py
+++ main_learning.py
@@ -21,15 +21,19 @@ print("LEARNING")
 logging.getLogger().setLevel(logging.INFO)
 logging.basicConfig(filename='logs', filemode='w', format='%(message)s')
 
-'''
+
 #plot functions
 def gr_dataset():
-    for lab, col in zip((0, 1),
-                        ('blue', 'red')):
+    for lab, col, text in zip((0, 1),
+                        ('blue', 'red'),
+                              ('real news', 'fake news')):
         plt.scatter(X_2d[y == lab, 0],
                     X_2d[y == lab, 1],
-                    label=lab,
-                    c=col)
+                    label=text,
+                    c=col,
+                    alpha=0.5)
+        plt.legend(loc="best")
+        plt.title("Scatterplot")
 
 def gr_membership_contour(estimated_membership):
     x = np.linspace(-4, 4, 50)
@@ -41,38 +45,42 @@ def gr_membership_contour(estimated_membership):
     membership_contour = plt.contour(X, Y, Z,
                                      levels=(.1, .3, .5, .95), colors='k')
     plt.clabel(membership_contour, inline=1)
-'''
+
 
 # data loading
 dataset = pd.read_csv("results/final_text_dataset.csv")
 
-dataset = dataset.head(400)
+#dataset = dataset.head(400)
 
 # start experiment
 e = Experiment(dataset)
 X, y = e.simple_split(dataset)
 X = [ast.literal_eval(i) for i in X]
 
-'''
+
 #reduce to 2d in order to plot
 pca_2d = PCA(n_components=2)
 X_2d = pca_2d.fit_transform(X)
 
 # plot data
+fig = plt.figure(figsize=(10, 10))
 gr_dataset()
 plt.show()
+fig.savefig("images/scatterplot.png")
 
 # try a basic model, fitted with X in 2d and see how the kernel works
-
 f = FuzzyInductor()
 f.fit(X_2d, y)
+
+fig = plt.figure(figsize=(10, 10))
 gr_dataset()
 gr_membership_contour(f.estimated_membership_)
 plt.show()
-'''
+fig.savefig("images/scatterplot_contour.png")
+
 
 #split in train and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X_2d, y, test_size=0.2, random_state = 42)
 
 # fit with training set
 f = FuzzyInductor()
