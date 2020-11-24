8cc5f50ae2fc15efb082351ebd5ec43171cf1b16
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2020-11-24 15:27:11 +0100

    select model function

--- classes/model.py
+++ classes/model.py
@@ -1,18 +1,78 @@
 import pandas as pd
 import os
 import pickle
+import ast
+import numpy as np
+from mulearn import FuzzyInductor, fuzzifier, kernel, optimization as opt
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
 
 from datetime import datetime
 
 class Model:
 
-    def __init__(self, fuzzy_inductor):
-        self.model = fuzzy_inductor
+    def __init__(self):
         self.date = datetime.now().strftime('%d.%m.%Y')
         self.time = datetime.now().strftime('%H.%M')
 
+    def select_model(self, dataset, outer_folds = 5, inner_folds = 4):
+        X, y = self.simple_split(dataset)
+        X = [ast.literal_eval(i) for i in X]  # this is needed to parse strings
+        X = np.array(X)
+
+        scalers = [StandardScaler(), RobustScaler(), MinMaxScaler()]
+
+        pipe = Pipeline([
+            ("scaler", None),
+            ("learning_algorithm", FuzzyInductor())
+        ])
+
+        sigmas = [.225, .5]
+        learning_params = {
+            'c': [1, 10, 100],
+            'k': [kernel.GaussianKernel(s) for s in sigmas]
+        }
+
+        params = {"scaler": scalers}
+
+        for k in learning_params:
+            params["learning_algorithm__" + k] = learning_params[k]
+
+        outer_fold = StratifiedKFold(n_splits=outer_folds)
+        scores = []
+        best_models = []
+        best_params = []
+
+        for i, (train_id, test_id) in enumerate(outer_fold.split(X, y)):
+            X_train, X_test = X[train_id], X[test_id]
+            y_train, y_test = y[train_id], y[test_id]
+
+            gs = GridSearchCV(
+                pipe,
+                params,
+                verbose=0,
+                cv=inner_folds,
+                scoring="neg_root_mean_squared_error"
+            )
+
+            gs = gs.fit(X_train, y_train)
+
+            score = gs.score(X_test, y_test)
+            scores.append(score)
+            best_models.append(gs.best_estimator_)
+            best_params.append(gs.best_params_)
+
+        self.best_models = best_models
+        self.best_params = best_params
+
+
+    def simple_split(self, dataset):
+        X = dataset.iloc[:, 0].values  # x-component
+        y = dataset.iloc[:, 1].values  # labels
+        return X, y
+
     def write_model(self):
-        params = self.extract_params()
         # 1. read to see if any dataframe is already available
         # 2. if yes, read it and update it
         # 3. if not, create it and fill it
@@ -28,27 +88,25 @@ class Model:
         try:
             models = pd.read_csv("selected_models/models.csv")
         except:
-            column_names = ["Date", "Time", "Model"]
+            column_names = ["Date", "Time", "Params"]
             models = pd.DataFrame(columns=column_names)
 
-        models = models.append({
-            "Date": self.date, "Time": self.time, "Model": params
-        }, ignore_index=True)
+        for k in self.best_params:
+            models = models.append({
+                "Date": self.date, "Time": self.time, "Params": k
+            }, ignore_index=True)
 
-        models.to_csv(fullname, index=False)
+            models.to_csv(fullname, index=False)
 
-        outdir = "selected_models/"
-        outname = "model_" + self.date + "_" + self.time
+        for i, model in enumerate(self.best_models):
+            outdir = "selected_models/"
+            outname = "model_" + self.date + "_" + self.time + "(" + str(i) + ")"
 
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
 
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
+            fullname = os.path.join(outdir, outname)
 
-        fullname = os.path.join(outdir, outname)
-
-        file = open(fullname, "wb")
-        pickle.dump(self, file)
-        file.close()
-
-    def extract_params(self):
-        return self.model.get_params()
+            file = open(fullname, "wb")
+            pickle.dump(model, file)
+            file.close()
--- main_model_selection.py
+++ main_model_selection.py
@@ -1,27 +1,13 @@
 import pandas as pd
-import ast
-import numpy as np
-from mulearn import FuzzyInductor
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
-from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
 
 from classes.model import Model
-from mulearn import FuzzyInductor, fuzzifier, kernel, optimization as opt
 
 # MODEL SELECTION
 
 ''' DESCRIPTION '''
-''' This is the file where models are selected by using the training set '''
-
-def simple_split(dataset):
-    X = dataset.iloc[:, 0].values  # x-component
-    y = dataset.iloc[:, 1].values  # labels
-    return X, y
+''' This is the file where models are selected by doing a nested cross validation '''
 
 PATH_TEXTS = "preprocessed_datasets/text/23.11.2020_02.22/final_text_dataset_38592.csv"
-outer_folds = 5
-inner_folds = 4
 
 # data loading
 dataset = pd.read_csv(PATH_TEXTS)
@@ -29,77 +15,10 @@ dataset = pd.read_csv(PATH_TEXTS)
 # extract sample
 dataset = dataset.head(10)
 
-X, y = simple_split(dataset)
-X = [ast.literal_eval(i) for i in X] # this is needed to parse strings
-X = np.array(X)
+# select best models
+m = Model()
+m.select_model(dataset)
 
-scalers = [StandardScaler(), RobustScaler(), MinMaxScaler()]
-
-pipe = Pipeline([
-    ("scaler", None),
-    ("learning_algorithm", FuzzyInductor())
-])
-
-sigmas = [.225,.5]
-learning_params = {
-    'c': [1,10,100],
-    'k': [kernel.GaussianKernel(s) for s in sigmas]
-}
-
-params = {"scaler": scalers}
-
-for k in learning_params:
-    params["learning_algorithm__" + k] = learning_params[k]
-
-print(params)
-
-outer_fold = StratifiedKFold(n_splits = outer_folds)
-scores = []
-best_models = []
-best_params = []
-
-for i, (train_id, test_id) in enumerate(outer_fold.split(X, y)):
-    X_train, X_test = X[train_id], X[test_id]
-    y_train, y_test = y[train_id], y[test_id]
-
-    gs = GridSearchCV(
-        pipe,
-        params,
-        verbose = 0,
-        cv = inner_folds,
-        scoring = "neg_root_mean_squared_error"
-    )
-
-    gs = gs.fit(X_train, y_train)
-
-    score = gs.score(X_test, y_test)
-    scores.append(score)
-    best_models.append(gs.best_estimator_)
-    best_params.append(gs.best_params_)
-
-print(best_models)
-print(best_params)
-
-# Whenever a model is selected, an instance of Model is created and then written
-f = FuzzyInductor()
-m = Model(f)
+# when the best models are found, they are serialized and stored
 m.write_model()
-
-''' Cross Validation - tuning of c and k'''
-'''
-sigmas = [.225,.5]
-parameters = {'c': [1,10,100],
-              'k': [kernel.GaussianKernel(s) for s in sigmas]}
-
-with warnings.catch_warnings():
-    warnings.simplefilter('ignore', FitFailedWarning)
-
-    crossval = GridSearchCV(f, param_grid=parameters)
-    crossval.fit(X_train, y_train)
-
-predictions = crossval.predict(X_test)
-e.set_params(crossval.get_params())
-print("Cross Validated error")
-print(e.RMSE(predictions, y_test))
-'''
 
