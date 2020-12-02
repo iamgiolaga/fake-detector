2c406a4cb57313fe79e4355836c841af6908b7af
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2020-12-02 12:34:09 +0100

    from experiments.csv to models.csv

--- main_results_viewer.py
+++ main_results_viewer.py
@@ -3,22 +3,22 @@ import pandas as pd
 ''' DESCRIPTION '''
 ''' This is the file where we can visualize the final results '''
 
-# 1. read to see if there exists a experiments.csv file
+# 1. read to see if there exists a models.csv file
 # 2. if yes, read it and store it in a dataframe
 # 3. if not, raise an exception
 from termcolor import colored
 
 try:
-    experiments = pd.read_csv("results/experiments.csv")
+    models = pd.read_csv("selected_models/models.csv")
 except:
     raise FileNotFoundError
 
-if experiments.empty:
-    print(colored("No experiments found", "red"))
+if models.empty:
+    print(colored("No models found", "red"))
 
 # TODO: prepare query that could be useful to retrieve results on some argument basis
 
-print(experiments.columns)
-print(experiments.head(10))
-print(experiments.iloc[0])
+print(models.columns)
+print(models.head(10))
+print(models.iloc[0])
 
