aa9680879254b4a41c8ab07f85b0ec80b8a52621
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2020-12-02 15:19:54 +0100

    main_use_model

---
+++ main_use_model.py
@@ -0,0 +1,16 @@
+import pickle
+from termcolor import colored
+
+fullname = "selected_models/02.12.2020_11.49/model_"
+models = []
+
+for i in range(0, 5):
+    print(colored("Downloading model " + str(i) + "...", "red"))
+    file = open(fullname + "(" + str(i) + ").pickle", "rb")
+    models.append(pickle.load(file))
+
+print(colored("Download completed!\n", "green"))
+
+for i in range(0, len(models)):
+    print("MODEL " + str(i) + ":")
+    print(models[i])
