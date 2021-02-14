b71354936c365f155b612ef8914f14f4051e9e70
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-02-14 16:58:38 +0100

    no more multiple generated files for syntetic experiments

--- main_use_preprocessed.py
+++ main_use_preprocessed.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 import pandas as pd
+import ast
 
 from classes.preprocessing import Preprocessing
 from datetime import datetime
@@ -10,45 +11,36 @@ from datetime import datetime
 
 ''' Generated data case '''
 
-PATH_TEXTS = "generated_text/31.01.2021_19.07/"
+PATH_TEXTS = "generated_text/14.02.2021_16.46/generated_dataset_500.csv"
 
-directory = os.fsencode(PATH_TEXTS)
+# directory = os.fsencode(PATH_TEXTS)
 
 date = datetime.now().strftime('%d.%m.%Y')
 time = datetime.now().strftime('%H.%M')
 
-generated_data = pd.DataFrame(columns=["text", "membership"])
+generated = pd.read_csv(PATH_TEXTS)
+print(generated)
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".csv"):
-        generated = pd.read_csv(os.path.join(PATH_TEXTS, filename))
-        del generated["Unnamed: 0"]
-        generated["text"] = generated[generated.columns[0:]].apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1)
-        generated["text"] = generated["text"].apply(lambda x: x.split(" "))
+generated["text"] = generated["text"].apply(lambda s: ast.literal_eval(s))
 
-        pp_generated = Preprocessing(
-            generated["text"],
-            date,
-            time,
-            analysis="text",
-            news_type="generated",
-            duplicate_rows_removal=False, lowercasing=False, tokenization=False,
-            lemmatization=False, noise_removal=False, stemming=False,
-            stopword_removal=False, entity_recognition=False, data_augmentation=False,
-            word2vec=True, doc2vec=False, aggregation=True
-        )  # here you can set the configuration
+pp_generated = Preprocessing(
+    generated,
+    date,
+    time,
+    analysis="text",
+    news_type="generated",
+    duplicate_rows_removal=False, lowercasing=False, tokenization=False,
+    lemmatization=False, noise_removal=False, stemming=False,
+    stopword_removal=False, entity_recognition=False, data_augmentation=False,
+    word2vec=True, doc2vec=False, aggregation=True
+)  # here you can set the configuration
 
-        gen = pp_generated.run_pipeline()
-        dataframe = pd.DataFrame(gen.aggregated)
-        dataframe["membership"] = filename.split("_")[1].replace(".csv", "")
-        generated_data = generated_data.append(dataframe)
-        continue
-    else:
-        continue
-
-preprocessing = Preprocessing(generated_data, date, time)
-dataset = preprocessing.shuffle(generated_data).reset_index(drop=True)
+gen = pp_generated.run_pipeline()
+dataframe = pd.DataFrame(gen.aggregated, columns = ["text"])
+dataframe["membership"] = generated["membership"]
+dataset = pp_generated.shuffle(dataframe).reset_index(drop = True)
+dataset.columns = ["text", "membership"]
+dataset.index.name = "index"
 
 cardinality = len(dataset)
 
